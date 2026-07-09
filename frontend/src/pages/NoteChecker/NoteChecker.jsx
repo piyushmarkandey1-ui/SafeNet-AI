5bd0a62f2e872c3d0c3f7510f9f1c9144770ca6c
Piyush Markandey <piyushmarkandey1@gmail.com>
2026-07-09 22:30:38 +0530

    fix(ui): remove redundant onClick handler on drop zone container to prevent double file picker launch

--- frontend/src/pages/NoteChecker/NoteChecker.jsx
+++ frontend/src/pages/NoteChecker/NoteChecker.jsx
@@ -270,7 +270,6 @@ export default function NoteChecker() {
                 onDrop={handleDrop}
                 onDragOver={handleDragOver}
                 onDragLeave={handleDragLeave}
-                onClick={() => fileInputRef.current?.click()}
               >
                 <input
                   ref={fileInputRef}
